7e8853344a38a93ddf234654780e31899dc2c0f8
Dave Barker <dave@davebarker.xyz>
2024-07-05 01:12:24 +0100

    Add support for extra hash algorithms

--- hashy.go
+++ hashy.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"errors"
 	"flag"
 	"fmt"
+	"hash"
 	"io"
 	"io/fs"
 	"os"
@@ -35,11 +39,14 @@ func main() {
 	var workerCount int
 	var exclude string
 	var versionFlag bool
+	var algorithm string
 
 	flag.IntVar(&workerCount, "workers", defaultWorkerCount, "")
 	flag.IntVar(&workerCount, "w", defaultWorkerCount, "")
 	flag.StringVar(&exclude, "exclude", "", "")
-	flag.StringVar(&exclude, "x", "", "s")
+	flag.StringVar(&exclude, "x", "", "")
+	flag.StringVar(&algorithm, "algorithm", "md5", "")
+	flag.StringVar(&algorithm, "a", "md5", "")
 	flag.BoolVar(&showAllErrors, "show-errors", false, "")
 	flag.BoolVar(&debug, "debug", false, "")
 	flag.BoolVar(&versionFlag, "version", false, "")
@@ -50,6 +57,12 @@ func main() {
 	if versionFlag {
 		fmt.Printf("hashy version: %s\n", version)
 		os.Exit(0)
+	}
+
+	// Check hash algorith is valid
+	_, hInitErr := getHasher(algorithm)
+	if hInitErr != nil {
+		util.Die("failed to initialise hasher: %s\n", hInitErr)
 	}
 
 	args := flag.Args()
@@ -80,7 +93,7 @@ func main() {
 
 	dPrint("\033[2J")
 
-	hashDir(dirPath, workerCount, excludeList)
+	hashDir(dirPath, workerCount, excludeList, algorithm)
 }
 
 func dPrint(format string, args ...any) bool {
@@ -92,31 +105,47 @@ func dPrint(format string, args ...any) bool {
 	}
 }
 
+func getHasher(algo string) (hash.Hash, error) {
+	switch algo {
+	case "md5":
+		return md5.New(), nil
+	case "sha1":
+		return sha1.New(), nil
+	case "sha256":
+		return sha256.New(), nil
+	case "sha512":
+		return sha512.New(), nil
+	}
+
+	return nil, fmt.Errorf("unsupported algorithm %s. see hashy -h for list of supported options", algo)
+}
+
 func printUsage() {
-	fmt.Printf(`Recursively every file in supplied path, writing the hash to stdout.
+	fmt.Printf(`Recursively hash every file in supplied path, writing the hash to stdout.
 
-Usage: hashy [-hv] [-w number] [-x path1,path2] path
+Usage: hashy [-hv] [-a algorithm] [-w number] [-x path1,path2] path
 
--h                        show this help message
--x,exclude path1,path1    comma-separated list of directories to exclude
--w,-workers number        number of workers
--v,-version               show version number
+-a,algorithm algorithm    hash algorithm: md5, sha1, sha256, sha512 (default: md5)
 -debug                    enable debug output
+-h                        show this help message
 -show-errors              show normally-suppressed errors (like skipping non-regular files)
+-v,-version               show version number
+-w,-workers number        number of workers (default: %d)
+-x,exclude path1,path1    comma-separated list of directories to exclude
 
-Example: hashy -x $HOME/Library,$HOME/.lima ~/
+Example: hashy -a sha256 -x $HOME/Library,$HOME/.lima ~/
 
-`)
+`, defaultWorkerCount)
 	os.Exit(0)
 }
 
-func hashDir(dirPath string, workerCount int, excludeList []string) {
+func hashDir(dirPath string, workerCount int, excludeList []string, algorithm string) {
 	jobs := make(chan string, workerCount)
 	wg := new(sync.WaitGroup)
 
 	for w := 0; w < workerCount; w++ {
 		wg.Add(1)
-		go hashWorker(w, jobs, wg)
+		go hashWorker(w, jobs, wg, algorithm)
 	}
 
 	filepath.WalkDir(dirPath, func(path string, dir fs.DirEntry, err error) error {
@@ -143,11 +172,11 @@ func hashDir(dirPath string, workerCount int, excludeList []string) {
 	wg.Wait()
 }
 
-func hashWorker(id int, jobs chan string, wg *sync.WaitGroup) {
+func hashWorker(id int, jobs chan string, wg *sync.WaitGroup, algorithm string) {
 	defer wg.Done()
 
 	for path := range jobs {
-		hash, err := hashFile(path, id)
+		hash, err := hashFile(path, id, algorithm)
 
 		var badFileErr *unsupportedFileError
 		if err == nil {
@@ -164,7 +193,7 @@ func hashWorker(id int, jobs chan string, wg *sync.WaitGroup) {
 	dPrint("%d: worker done", id)
 }
 
-func hashFile(path string, id int) (string, error) {
+func hashFile(path string, id int, algorithm string) (string, error) {
 	// Check if file is regular file
 	// We have to do this every time despite the performance impact as named piped will
 	// "open" and can be read with io.Copy but block the goroutine and never finish reading
@@ -191,7 +220,11 @@ func hashFile(path string, id int) (string, error) {
 	defer f.Close()
 
 	dPrint("%d: hashing %s", id, path)
-	hasher := md5.New()
+	hasher, err := getHasher(algorithm)
+	if err != nil {
+		util.Die("failed to initialise hasher: %s\n", err)
+	}
+
 	if _, err := io.Copy(hasher, f); err != nil {
 		util.Die("Error reading %s: %s\n", path, err)
 	}
